044034bc02960e3e6fd75c96482a95f680185445
naman-preet <preet.2005.naman@gmail.com>
2025-09-10 09:07:05 +0530

    initialized

---
+++ day2/day4/index.js
@@ -0,0 +1,31 @@
+//output method:
+console.log("FSD class");
+//input method:
+// let  a=promt("enter a number:");
+// console.log(a);
+//javascript must be write in the camel case:use let is prefer in jsgb 
+
+//diffrence between var let and const(interview percepective)
+// properties:redeclare:reassignment:Scope
+// var a =20;
+// var a =56;
+// let a =45;
+// let a=67;//we cannot redeclare in let 
+
+// //reassign
+// let b=10;//for reassign they must be in block
+// console.log("after first assignment of b's value"+b);
+
+// b-90;
+// console.log("after  assignment of b's value"+b);
+// {
+//     let name="priya";
+//     name="xyz";
+//     console.log("name:",+name);
+
+// }
+//strings 
+let firstname="priya ";
+let lastname="sharma";
+let meassage="hello";
+let outputMessage="";
