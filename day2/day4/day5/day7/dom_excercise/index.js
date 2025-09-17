cd4620aef6e3987918b977f47e92beba570f008e
naman-preet <preet.2005.naman@gmail.com>
2025-09-17 10:06:47 +0530

    new

--- day2/day4/day5/day7/dom_excercise/index.js
+++ day2/day4/day5/day7/dom_excercise/index.js
@@ -1,16 +1,11 @@
-      let ison=true;
-      function colorHandler(color){
-        const bulb=document.getElementById("bulb");
-     if(ison){
-        console.log(color);
-        console.log(document.getElementById("bulb"));
-        bulb.style.backgroundColor=color;
-        ison=false; 
-            }
-            else{
-                bulb.style.backgroundColor="grey";
-                ison=true;}
-            // console.log(color);
-            // const bulb=document.getElementById("bulb");
-            // bulb.style.backgroundColor=color;
+ let ison = true; // control enable/disable
+
+    function colorHandler(color) {
+      const bulb = document.getElementById("bulb");
+
+      if (ison) {
+        bulb.style.backgroundColor = color;
+      } else {
+        alert("Bulb is disabled! Click Enable to use it again.");
+      }
     }
